3db6ae67dee9f302fbf54e7bb4ab72ce375e4691
Heinson King <HeinsonJ.King@pccw.com>
2018-08-29 12:15:03 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,9 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Heinson',
-        text: 'Yup, that works for me.'    
-    });
+    console.log('Connected to server');    
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
-
-socket.on('newMessage', function (message) {
-    console.log('New message', message);
-})
